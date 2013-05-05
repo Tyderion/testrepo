d4c4b65d12bd927ce1fb92db2579933836f93e0e
Gabriel Nadler <nadlergabriel@gmail.com>
2013-05-05 11:47:52 +0200

    better

--- install1.sh
+++ install1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 dir="/usr/local/samba"
 host=`hostname`
 pcname="vmADC"
@@ -7,26 +8,17 @@ interface="eth0"
 gateway="192.168.5.1"
 ip="192.168.5.10"
 netmask="255.255.255.0"
+echo "Updating and installing prerequeisites"
+apt-get update
+apt-get install -y build-essential libacl1-dev libattr1-dev libblkid-dev libgnutls-dev libreadline-dev python-dev python-dnspython gdb pkg-config libpopt-dev libldap2-dev dnsutils libbsd-dev attr krb5-user docbook-xsl libcups2-dev libncurses5-dev git
+echo "Downloading samba4"
+git clone git://git.samba.org/samba.git samba4
 echo "Configureing PC $host as $pcname.$domain"
 newnet="iface $interface inet static\naddress $ip\nnetmask $netmask\ngateway $gateway\ndns-nameservers $ip\ndns-search $domain"
 netregex=".*iface $interface inet dhcp.*"
-echo "Regplacing $netregex with $newnet"
 sed -i.bak "s/$netregex/$newnet/" /etc/network/interfaces
 echo "Replacing $host with $pcname.$domain"
-sudo cp /etc/hostname{,.bak}
-sudo sh -c 'echo vmADC.fiktiv.local > /etc/hostname'
+sed -i.bak "s/$host/$pcname.$domain" /etc/hostname
 sed -i.bak "/127.0./ s/$host/$pcname.$domain $pcname/" /etc/hosts
 ifdown eth0 && ifup eth0
 echo "Installing samba4 prerequeistes"
-#apt-get update
-#apt-get install -y build-essential libacl1-dev libattr1-dev libblkid-dev libgnutls-dev libreadline-dev python-dev python-dnspython gdb pkg-config libpopt-dev libldap2-dev dnsutils libbsd-dev attr krb5-user docbook-xsl libcups2-dev libncurses5-dev git
-
-#git clone git://git.samba.org/samba.git samba4
-#cd samba4
-#./configure --enable-debug --enable-selftest --$dir
-
-#make
-
-#make install
-
-#/usr/local/samba/bin/samba-tool domain provision
---
+++ undo_network.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp /etc/hostname.bak /etc/hostname
+cp /etc/network/interfaces.bak /etc/network/interfaces
