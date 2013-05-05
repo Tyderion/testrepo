74698dde64ca64ae1f985c46ece9b2b609061cb8
Gabriel Nadler <nadlergabriel@gmail.com>
2013-05-05 14:33:07 +0200

    fixed regex

--- install3.sh
+++ install3.sh
@@ -3,4 +3,4 @@ domain="fiktiv.local"
 echo "Configure kerberos"
 mv /etc/krb5.conf /etc/krb.conf.bak
 cp /usr/local/samba/share/setup/krb5.conf /etc/krb5.conf
-sed -i.bak2 "s/${REALM}/$domain/" /etc/krb5.conf
+sed -i.bak2 "s/\${REALM}/$domain/" /etc/krb5.conf
